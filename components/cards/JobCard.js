ea7d4715e67c84cfd8bc1196fefb2d4c964af8bb
yzhedwin <zizo_edwin@hotmail.com>
2025-06-29 17:22:42 +0000

    fix: width changing due to parent div expansion

--- components/cards/JobCard.js
+++ components/cards/JobCard.js
@@ -34,89 +34,97 @@ export default function JobCard(props) {
   };
 
   return (
-    <Card
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 1,
-        textAlign: "left",
-        boxShadow: 3,
+    <motion.div
+      animate={{
+        height: expanded ? "100%" : "40vh", // Use a single base value or calculate manually
+      }}
+      transition={{ duration: 0.2, ease: "easeInOut" }}
+      style={{
         overflow: "hidden",
-        height: { sx: expanded ? "100%" : "35vh", sm: expanded ? "100%" : "50vh", md: expanded ? "100%" : "40vh" },
-        width: "100%",
       }}
     >
-      <CardHeader
-        avatar={<Avatar alt="logo" src={companyLogo} ></Avatar>}
-        action={
-          <Typography
-            title={`Visit ${companyName} Webpage`}
-            variant="h6"
-            noWrap
-            component="a"
-            href={linkToCompany}
+      <Card
+        sx={{
+          overflow: "hidden",
+          height: "100%",
+          boxShadow: 3,
+          textAlign: "left",
+          padding: 1,
+          width: { sx: "100vw", md: "30vw", lg: "35vw" },
+        }}
+      >
+        <CardHeader
+          avatar={<Avatar alt="logo" src={companyLogo} ></Avatar>}
+          action={
+            <Typography
+              title={`Visit ${companyName} Webpage`}
+              variant="h6"
+              noWrap
+              component="a"
+              href={linkToCompany}
 
-          >
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
+            >
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            </Typography>
+          }
+          title={companyName}
+          subheader={jobTitle}
+          sx={{ height: "13vh", textAlign: "left", fontSize: { xs: "1.2rem", sm: "1.5rem" } }}
+        />
+        <CardMedia component="img" image={imageURL} alt={jobType}
+          sx={{ height: { sm: "35vh", md: "15vh" }, objectFit: "scale-down" }} />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {jobSummary ? jobSummary : "No summary available."}
           </Typography>
-        }
-        title={companyName}
-        subheader={jobTitle}
-        sx={{ height: "13vh", textAlign: "left", fontSize: { xs: "1.2rem", sm: "1.5rem" } }}
-      />
-      <CardMedia component="img" image={imageURL} alt={jobType}
-        sx={{ height: { sm: "35vh", md: "15vh" }, objectFit: "scale-down" }} />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          {jobSummary ? jobSummary : "No summary available."}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <LightTooltip title="See more">
-            <ExpandMoreIcon sx={{ cursor: "grab" }} />
-          </LightTooltip>
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <motion.div
-          key="desc"
-          initial={{ opacity: 0, height: 0 }}
-          animate={{ opacity: 1, height: "auto" }}
-          exit={{ opacity: 0, height: 0 }}
-          transition={{ type: "spring", stiffness: 200, damping: 10 }}
-          style={{ overflow: "hidden" }} // crucial to prevent content overflow during animation
-        >
-          <CardContent>
-            <Typography paragraph>
-              <b>Description:</b>
-            </Typography>
-            {Array.isArray(jobDescriptionList) && jobDescriptionList.length > 0 ? (
-              jobDescriptionList.map((desc, index) => (
-                <Typography key={index} paragraph>
-                  {desc}
-                </Typography>
-              ))
-            ) : (
+        </CardContent>
+        <CardActions disableSpacing>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <LightTooltip title="See more">
+              <ExpandMoreIcon sx={{ cursor: "grab" }} />
+            </LightTooltip>
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <motion.div
+            key="desc"
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: "auto" }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{ type: "spring", stiffness: 200, damping: 10 }}
+            style={{ overflow: "hidden" }} // crucial to prevent content overflow during animation
+          >
+            <CardContent>
               <Typography paragraph>
-                No specific job experiences listed.
+                <b>Description:</b>
               </Typography>
-            )}
+              {Array.isArray(jobDescriptionList) && jobDescriptionList.length > 0 ? (
+                jobDescriptionList.map((desc, index) => (
+                  <Typography key={index} paragraph>
+                    {desc}
+                  </Typography>
+                ))
+              ) : (
+                <Typography paragraph>
+                  No specific job experiences listed.
+                </Typography>
+              )}
 
-            <Typography paragraph>
-              <b>Frameworks Used:</b>
-            </Typography>
-            <Typography paragraph>{frameworkList ?? "N/A"}</Typography>
-          </CardContent>
-        </motion.div>
-      </Collapse>
-    </Card>
+              <Typography paragraph>
+                <b>Frameworks Used:</b>
+              </Typography>
+              <Typography paragraph>{frameworkList ?? "N/A"}</Typography>
+            </CardContent>
+          </motion.div>
+        </Collapse>
+      </Card>
+    </motion.div>
   );
 }
