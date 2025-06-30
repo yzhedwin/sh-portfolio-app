63c1f1cd1166a631e38211ac8cfac3b379684040
Edwin Yeo <69493564+yzhedwin@users.noreply.github.com>
2025-06-30 21:53:57 +0800

    Refactor/use tailwind and framer motion (#7)
    * fix: x overflow
    
    * fix: animation not executing smoothly
    
    * fix: width changing due to parent div expansion
    
    * feat: add overlay when click on internship experience
    
    * fix: merge conflict
    
    * fix: tooltip title conflict

--- components/Internship.js
+++ components/Internship.js
@@ -1,7 +1,7 @@
-import { motion, useInView } from "framer-motion";
-import { useRef } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import JobCard from "./cards/JobCard";
-import { DSTA_INTERNSHIP_COMPANY, DSTA_INTERNSHIP_COMPANY_LOGO, DSTA_INTERNSHIP_COMPANY_URL, DSTA_INTERNSHIP_DESCRIPTION, DSTA_INTERNSHIP_DESCRIPTION_2, DSTA_INTERNSHIP_DESCRIPTION_3, DSTA_INTERNSHIP_DESCRIPTION_4, DSTA_INTERNSHIP_FRAMEWORKS, DSTA_INTERNSHIP_IMAGE_URL, DSTA_INTERNSHIP_JOB_TITLE, DSTA_INTERNSHIP_JOB_TYPE, DSTA_INTERNSHIP_SUMMARY, SI_INTERNSHIP_COMPANY, SI_INTERNSHIP_COMPANY_LOGO, SI_INTERNSHIP_COMPANY_URL, SI_INTERNSHIP_DESCRIPTION, SI_INTERNSHIP_DESCRIPTION_2, SI_INTERNSHIP_DESCRIPTION_3, SI_INTERNSHIP_DESCRIPTION_4, SI_INTERNSHIP_FRAMEWORKS, SI_INTERNSHIP_IMAGE_URL, SI_INTERNSHIP_JOB_TITLE, SI_INTERNSHIP_JOB_TYPE, SI_INTERNSHIP_SUMMARY } from "./Constants";
+import { INTERNSHIP_CARDS } from "./Constants";
 
 const card3DVariants = {
     initial: { opacity: 0, rotateY: 45, scale: 0.9 },
@@ -9,39 +9,63 @@ const card3DVariants = {
 };
 
 export default function Internship({ isInView }) {
+    const [card, setCard] = useState();
+
     return (
         <div className="grid md:grid-cols-2 gap-8">
-            <div className="bg-red p-6 rounded-2xl shadow-2xl transform-style-preserve-3d">
-                <JobCard
-                    imageURL={SI_INTERNSHIP_IMAGE_URL}
-                    companyLogo={SI_INTERNSHIP_COMPANY_LOGO}
-                    companyName={SI_INTERNSHIP_COMPANY}
-                    jobTitle={SI_INTERNSHIP_JOB_TITLE}
-                    jobType={SI_INTERNSHIP_JOB_TYPE}
-                    jobSummary={SI_INTERNSHIP_SUMMARY}
-                    linkToCompany={SI_INTERNSHIP_COMPANY_URL}
-                    jobDescriptionList={[
-                        SI_INTERNSHIP_DESCRIPTION, SI_INTERNSHIP_DESCRIPTION_2, SI_INTERNSHIP_DESCRIPTION_3, SI_INTERNSHIP_DESCRIPTION_4
-                    ]}
-                    frameworkList={SI_INTERNSHIP_FRAMEWORKS}
-                />
-            </div>
+            {INTERNSHIP_CARDS.map((card) => (
+                <motion.div
+                    key={card.id}
+                    layoutId={card.id}
+                    className="card bg-red p-6 rounded-2xl shadow-2xl transform-style-preserve-3d"
+                    onClick={() => setCard(card)}
+                >
+                    <JobCard
+                        expanded={false}
+                        imageURL={card.imageURL}
+                        companyLogo={card.companyLogo}
+                        companyName={card.companyName}
+                        jobTitle={card.jobTitle}
+                        jobType={card.jobType}
+                        jobSummary={card.jobSummary}
+                        linkToCompany={card.linkToCompany}
+                        jobDescriptionList={card.jobDescriptionList}
+                        frameworkList={card.frameworkList}
+                        card={card}
+                    />
+                </motion.div>
+            ))}
+            {/* To fix screen on phones */}
+            <AnimatePresence>
+                {card && (
+                    <motion.div
+                        layoutId={card.id}
+                        className="fixed inset-0 top-60 sm:top-0 flex items-center justify-center"
+                        onClick={() => setCard(null)}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                    >
 
-            <div className="bg-blue p-6 rounded-2xl shadow-2xl transform-style-preserve-3d">
-                <JobCard
-                    imageURL={DSTA_INTERNSHIP_IMAGE_URL}
-                    companyLogo={DSTA_INTERNSHIP_COMPANY_LOGO}
-                    companyName={DSTA_INTERNSHIP_COMPANY}
-                    jobTitle={DSTA_INTERNSHIP_JOB_TITLE}
-                    jobType={DSTA_INTERNSHIP_JOB_TYPE}
-                    jobSummary={DSTA_INTERNSHIP_SUMMARY}
-                    linkToCompany={DSTA_INTERNSHIP_COMPANY_URL}
-                    jobDescriptionList={[
-                        DSTA_INTERNSHIP_DESCRIPTION, DSTA_INTERNSHIP_DESCRIPTION_2, DSTA_INTERNSHIP_DESCRIPTION_3, DSTA_INTERNSHIP_DESCRIPTION_4
-                    ]}
-                    frameworkList={DSTA_INTERNSHIP_FRAMEWORKS}
-                />
-            </div>
+                        <div className="scale-y-[0.8] sm:scale-100 origin-top">
+
+                            <JobCard
+                                expanded={true}
+                                imageURL={card.imageURL}
+                                companyLogo={card.companyLogo}
+                                companyName={card.companyName}
+                                jobTitle={card.jobTitle}
+                                jobType={card.jobType}
+                                jobSummary={card.jobSummary}
+                                linkToCompany={card.linkToCompany}
+                                jobDescriptionList={card.jobDescriptionList}
+                                frameworkList={card.frameworkList}
+                            />
+
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 }
--- pages/About.js
+++ pages/About.js
@@ -1,125 +1,92 @@
 import { Avatar, Grow, Slide, Typography, Zoom } from "@mui/material";
-import React, { Component } from "react";
+import React, { Component, useRef } from "react";
 import { Waypoint } from "react-waypoint";
 import { HtmlTooltip } from "../components/ToolTips";
+import { useInView, motion } from "framer-motion";
 
-class About extends Component {
-  render() {
-    return (
-      <>
-        <Waypoint
-          onEnter={this.props._titleEnter}
-          onLeave={this.props._titleExit}
-          topOffset={"10%"}
-          bottomOffset={"10%"}
+export default function About() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 0.3, once: false });
+  return (
+    <section ref={ref} className="w-full py-20 px-4 perspective-1000 flex flex-col justify-center items-center w-full min-h-[100vh]">
+      <motion.div
+        initial={{ x: 0, y: -300, opacity: 0 }}
+        animate={isInView ? { x: 0, y: 0, opacity: 1 } : {}}
+        transition={{ duration: 1 }}
+        className="text-4xl font-bold text-center mb-12"
+      >
+        <h4>About me</h4>
+      </motion.div>
+      <div className="flex justify-around items-center w-full">
+        <motion.div
+          initial={{ x: -300, opacity: 0 }}
+          animate={isInView ? { x: 0, opacity: 1 } : {}}
+          transition={{ duration: 1.5 }}
+          className="text-4xl font-bold text-center mb-12"
         >
-          <div className="title">
-            <Slide
-              mountOnEnter
-              unmountOnExit
-              in={this.props.aboutTitle}
-              direction="right"
-              timeout={{ enter: 1000, exit: 200 }}
-            >
-              <h4>About me</h4>
-            </Slide>
-          </div>
-        </Waypoint>
-        <div className="about-introcontainer">
-          <Waypoint
-            onEnter={this.props._profileEnter}
-            onLeave={this.props._profileExit}
-            topOffset={"25%"}
-            bottomOffset={"25%"}
-          >
-            <div className="about-profile">
-              <Zoom
-                in={this.props.aboutProfile}
-                timeout={500}
-                mountOnEnter
-                unmountOnExit
-              >
-                <div>
-                  <HtmlTooltip
-                    title={
-                      <>
-                        <Typography color="inherit">
-                          <em>Edwin Yeo</em>
-                        </Typography>
-                        {"Click to view my LinkedIn Profile!"}
-                      </>
-                    }
-                  >
-                    <div>
-                      <Typography
-                        title="LinkedIn"
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="https://www.linkedin.com/in/yzhedwin/"
-                      >
-                        <Avatar
-                          alt="me"
-                          src="/assets/me.jpg"
-                          sx={{ width: 150, height: 150 }}
-                        />
-                      </Typography>
-                    </div>
-                  </HtmlTooltip>
-                </div>
-              </Zoom>
-            </div>
-          </Waypoint>
-          <div className="about-school">
-          <Zoom
-            in={this.props.aboutProfile}
-            style={{
-              transitionDelay: this.props.aboutProfile ? "500ms" : "200ms",
-            }}
-            timeout={1000}
-            mountOnEnter
-            unmountOnExit
+          <HtmlTooltip
+            title={
+              <>
+                <Typography color="inherit">
+                  <em>Edwin Yeo</em>
+                </Typography>
+                {"Click to view my LinkedIn Profile!"}
+              </>
+            }
           >
             <div>
-              <HtmlTooltip
-                title={
-                  <>
-                    <b>Click to visit NUS Webpage!</b>
-                  </>
-                }
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="https://www.linkedin.com/in/yzhedwin/"
               >
-                <div>
-                  <Typography
-                    title="NUS"
-                    variant="h6"
-                    noWrap
-                    component="a"
-                    href="https://www.nus.edu.sg/"
-                  >
-                    <img src="/assets/nus.jpg" width={150} alt="NUS" />
-                  </Typography>
-                </div>
-              </HtmlTooltip>
+                <Avatar
+                  alt="me"
+                  src="/assets/me.jpg"
+                  sx={{ width: 150, height: 150 }}
+                />
+              </Typography>
             </div>
-          </Zoom>
-          </div>
-        </div>
-        <Zoom
-          in={this.props.aboutProfile}
-          style={{
-            transitionDelay: this.props.aboutProfile ? "1000ms" : "200ms",
-          }}
-          timeout={1000}
-          mountOnEnter
-          unmountOnExit
+          </HtmlTooltip>
+        </motion.div>
+
+        <motion.div
+          initial={{ x: 300, opacity: 0 }}
+          animate={isInView ? { x: 0, opacity: 1 } : {}}
+          transition={{ duration: 1.5 }}
+          className="text-4xl font-bold text-center mb-12"
         >
-          <div className="intro">
-            I am a Year 3 Computer Engineering major at NUS
-          </div>
-        </Zoom>
-      </>
-    );
-  }
+          <HtmlTooltip
+            title={
+              <>
+                <b>Click to visit NUS Webpage!</b>
+              </>
+            }
+          >
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="https://www.nus.edu.sg/"
+            >
+              <img src="/assets/nus.jpg" width={150} alt="NUS" />
+            </Typography>
+          </HtmlTooltip>
+        </motion.div>
+      </div>
+      <motion.div
+        initial={{ x: 0, y: 300, opacity: 0 }}
+        animate={isInView ? { x: 0, y: 0, opacity: 1 } : {}}
+        transition={{ duration: 2 }}
+        className="text-4xl font-bold text-center mb-12"
+      >
+        {/*todo add letter by letter animation */}
+        <div className="mt-20 text-xxl text-white">
+          I am a Year 3 Computer Engineering major at NUS
+        </div>
+      </motion.div>
+    </section>
+  );
 }
 
-export default About;
